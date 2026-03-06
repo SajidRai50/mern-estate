f2ed7c0f740ec77cae9cb8931693c75c8eadf99a
RaiSajid50 <raisajid94@gmail.com>
2026-03-06 09:11:42 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import {BrowserRouter,Routes ,Route} from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SigIn from "./pages/SigIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <div className='bg-amber-300it'>App</div>
+   <BrowserRouter>
+   <Header/>
+   <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SigIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
